faff626bcead71ebcc41a7b773c3c4f9d2f0eb13
Sohit kumar Shivhare <hitso.shivhare@gmail.com>
2022-05-31 20:36:11 +0530

    for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
      // mongoURI: "mongodb://localhost:27017/panditconnectorDB"
+
+     
      mongoURI: "mongodb+srv://hitso:WvwSkYSPbHLUfIaQ@cluster0.xn90s68.mongodb.net/panditconnectorDB"
 };
