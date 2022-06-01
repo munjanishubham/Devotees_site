fb5aaa897c397ff32036b17a5f8d7b3008d20c7a
Sohit kumar Shivhare <hitso.shivhare@gmail.com>
2022-06-01 11:20:23 +0530

    bugs fixing in deployment

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
-     // mongoURI: "mongodb://localhost:27017/panditconnectorDB"
-
-     
-     mongoURI: "mongodb+srv://hitso:WvwSkYSPbHLUfIaQ@cluster0.xn90s68.mongodb.net/panditconnectorDB"
+     // mongoURI: "mongodb://localhost:27017/panditconnectorDB"     
+     mongoURI: process.env.URI
 };
